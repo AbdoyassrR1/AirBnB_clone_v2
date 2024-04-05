693023c653554bbab86cd9b31b502aed3ff45863
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-04-05 03:37:02 +0200

    fix hosts

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,7 +8,7 @@ import os
 do_pack = __import__('1-pack_web_static').do_pack
 # do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
-env.hosts = ['3.235.198.120', '3.239.50.204']
+env.hosts = ['54.174.245.241', '54.237.92.0']
 
 
 def deploy():
