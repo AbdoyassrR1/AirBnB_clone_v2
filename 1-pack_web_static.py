5d7a57eca2e93c1901f1112340bbe3af5a577ecf
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-04-05 02:51:32 +0200

    fix syntax

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 """
-This module contains a Fabric script that generates a .tgz
-archive from the contents of the web_static folder.
+fabric script that create a .tgz archive from
+the conten of the web_static folder.
 """
 from fabric.api import local
 from datetime import datetime
@@ -9,11 +10,10 @@ from datetime import datetime
 
 def do_pack():
     """
-    Compresses the contents of the web_static folder into a .tgz archive.
-
+    Compresse content of the web_static folder into a .tgz.
     Returns:
-            str: The path to the generated archive
-            if successful, None otherwise.
+            The path to the generated archive
+            None otherwise.
     """
     try:
         now = datetime.now()
