543b45fcb9c358887d30d61f15ed3b56a85316df
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-04-25 03:44:25 +0200

    update routes

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,7 +25,7 @@ def c_is_fun(text):
     return f"C {msg}"
 
 
-@app.route("/python")
+@app.route("/python/")
 @app.route("/python/<text>")
 def python_is_cool(text="is cool"):
     """print C {msg}"""
