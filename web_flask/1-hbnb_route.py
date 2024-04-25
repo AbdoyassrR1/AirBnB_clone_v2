6616043440002a4a8b38d79f4c60228e65ab67e6
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-04-25 03:32:42 +0200

    add hbnb route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application:
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_bnb():
+    """hello bnb"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """hello bnb"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    app.url_map.strict_slashes = False
